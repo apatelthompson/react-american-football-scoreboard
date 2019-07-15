1af03471fc7ffc6a82c33b77b5add543b968ac7d
Avni Patel Thompson <avnipatelthompson@Avnis-iMac.local>
2019-07-15 11:32:33 -0700

    added stretch goal of change quarter button

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const BottomRow = () => {
+  const [quarter, setNextQuarter] = useState(1);
+
   return (
     <div className="bottomRow">
       <div className="down">
@@ -18,7 +20,13 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{quarter}</div>
+        <button
+          className="quarter-button"
+          onClick={() => setNextQuarter(quarter + 1)}
+        >
+          Change Quarter
+        </button>
       </div>
     </div>
   );
